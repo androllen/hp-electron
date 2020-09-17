b0d1c8bde704e1b76eb9c572ab4f9543925ca59e
androllen <androllen@hotmail.com>
2020-09-17 13:12:24 +0800

    close install vue devtools

--- src/background.js
+++ src/background.js
@@ -64,11 +64,11 @@ app.on('activate', () => {
 app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
-    try {
-      await installExtension(VUEJS_DEVTOOLS)
-    } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
-    }
+    // try {
+    //   await installExtension(VUEJS_DEVTOOLS)
+    // } catch (e) {
+    //   console.error('Vue Devtools failed to install:', e.toString())
+    // }
   }
   createWindow()
 })
