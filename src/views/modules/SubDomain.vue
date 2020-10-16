c3af9f14a6e5ec6a923efc46305278d6607b76e0
androllen <androllen@hotmail.com>
2020-10-16 15:27:18 +0800

    update subdomain

--- src/views/modules/SubDomain.vue
+++ src/views/modules/SubDomain.vue
@@ -2,17 +2,10 @@
   <div>
     <div id="target">
       <div id="_t_input">
-        <el-input
-          ref="inputName"
-          v-model="m_target"
-          :disabled="m_disable"
-          placeholder="请输入内容">
-		</el-input>
+        <el-input ref="inputName" v-model="m_target" :disabled="m_disable" placeholder="请输入内容"> </el-input>
       </div>
       <div id="_t_input_btn">
-        <el-button type="primary" :disabled="m_disable" @click="onStart">
-			开始
-		</el-button>
+        <el-button type="primary" :disabled="m_disable" @click="onStart"> 开始 </el-button>
         <el-button @click="onStop">停止</el-button>
       </div>
     </div>
@@ -20,8 +13,8 @@
       <p><strong>扫描结果</strong></p>
       <div>
         <el-table :data="m_tableData" style="width: 100%">
-          <el-table-column prop="ip" label="IP" width="200" header-align="center"></el-table-column>
-          <el-table-column prop="CNAME" label="域名" header-align="center"></el-table-column>
+          <el-table-column prop="ip" label="IP" width="200" ></el-table-column>
+          <el-table-column prop="url" label="域名" ></el-table-column>
         </el-table>
       </div>
     </div>
@@ -29,13 +22,14 @@
 </template>
 
 <script type="text/javascript">
-import { GUID } from "../../utils";
-import ZmqJs from "../../service/zmq";
+import { GUID } from '../../utils';
+import ZmqJs from '../../service/zmq';
+import { ModeSubDomain } from '../../model';
 
 var _data = {
-  m_target: "bernama.com",
+  m_target: 'bernama.com',
   m_disable: false,
-  m_result: "",
+  m_tableData: [],
 };
 
 export default {
@@ -52,12 +46,46 @@ export default {
       this.m_disable = true;
       var task = {
         id: GUID(),
-        scriptid: "waf_check",
-        parameters: { url: this.m_target },
+        scriptid: 'subdomain',
+        parameters: {
+          url: this.m_target,
+          mode: '01',
+          flag: '11',
+        },
       };
-      this.m_result = "";
+      this.m_tableData = [];
       console.log(task);
 
+      ZmqJs.HandleSend(task, (topic) => {
+        try {
+          var index = topic.indexOf(',');
+          var id = topic.substring(0, index - 1).trim();
+          var json = topic.substring(index + 1).trim();
+         
+          if (json.startsWith('{') && json.endsWith('}')) {
+            console.log('this ia public data');
+            var obj = JSON.parse(json);
+            console.log(obj);
+            let ipstr = '';
+            obj.ip.forEach((element) => {
+              ipstr += element;
+            });
+
+            console.log(ipstr);
+
+            let temp = new ModeSubDomain(obj.url, ipstr);
+            this.m_tableData.push(temp);
+          } else if (json == 'end!!!') {
+            console.log('end!!!');
+          } else if (json.StartsWith('error_')) {
+            console.log('error_');
+          }
+        } catch (e) {
+          console.log('error..' + e);
+        } finally {
+          this.m_disable = false;
+        }
+      });
     },
     onStop() {
       this.m_disable = false;
@@ -90,5 +118,4 @@ export default {
 p {
   float: left;
 }
-
 </style>
