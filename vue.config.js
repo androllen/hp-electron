ae79019d981a52891b064c5e0e634145edc1f2c4
androllen <androllen@hotmail.com>
2020-11-13 19:16:31 +0800

    suggest build package method

--- vue.config.js
+++ vue.config.js
@@ -2,55 +2,21 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
-        "appId": "com.4dogs.tx",
+        appId: "com.4dogs.tx",
         // app.exe name
-        "productName": "TxMain",
-        "copyright": "Copyright © 2020",
-        "directories": {
-          "output": "./dist_electron"
-        },
+        productName: "TxMain",
         // options placed here will be merged with default configuration and passed to electron-builder
-        "files": [
+        files: [
           "**/*",
         ],
-        "extraFiles": [
+        extraFiles: [
           {
             "from": "TXThrift",
             "to": "./resources/TXThrift",
             "filter": ["**/*"]
           }
         ],
-        "nsis": {
-          "oneClick": false,
-          "perMachine": true,
-          "guid": "7e51495b-3f4d-5235-aadd-5636863064f1",
-          "allowElevation": true,
-          "allowToChangeInstallationDirectory": true,
-          //installer ico
-          "installerIcon": "./public/nsis/installer.ico",
-          //uninstaller ico
-          "uninstallerIcon": "./public/nsis/uninstaller.ico",
-          "installerHeaderIcon": "./public/nsis/installer.ico",
-          "createDesktopShortcut": true,
-          "createStartMenuShortcut": true,
-          //shortcut name
-          "shortcutName": "天象综合渗透测试平台",
-          "license": "./public/nsis/LICENSE",
-        },
-        "win": {
-          // exe ico
-          "icon": "./public/app.ico",
-          "target": [
-            {
-              "target": "nsis",
-              "arch": [
-                "x64",
-                "ia32"
-              ]
-            }
-          ]
-        },
-        "asar": false
+        asar: false
       },
       // List native deps here if they don't work
       externals: ['sqlite3', 'zeromq'],
