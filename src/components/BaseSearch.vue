983e0d32bdd7e4a8ad86431d37caaf9243866707
androllen <androllen@hotmail.com>
2020-11-20 17:22:46 +0800

    update search ui

--- src/components/BaseSearch.vue
+++ src/components/BaseSearch.vue
@@ -13,49 +13,35 @@
 
 <script>
 import Bus from '../router/bus';
+import getPageRoutes from '../router/view';
 
 export default {
   data() {
     return {
-      restaurants: [],
+      pageroutes: [],
       state: '',
     };
   },
   methods: {
     querySearch(queryString, cb) {
-      var restaurants = this.restaurants;
-      var results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants;
-      // 调用 callback 返回建议列表的数据
+      var pageroutes = this.pageroutes;
+      var results = queryString ? pageroutes.filter(this.createFilter(queryString)) : pageroutes;
+      // // 调用 callback 返回建议列表的数据
       cb(results);
     },
     createFilter(queryString) {
-      return (restaurant) => {
-        return restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0;
+      return (pageroutes) => {
+        return pageroutes.value.toLowerCase().indexOf(queryString.toLowerCase()) != -1;
       };
-    },
-    loadAll() {
-      return [
-        { value: 'Cdn 检测', address: '/info_cdn' },
-        { value: 'Waf 检测', address: '/info_waf' },
-        { value: '邮箱地址收集', address: '/info_email' },
-        { value: '子域名爆破', address: '/info_subdomain' },
-        { value: 'WhoIs 反查', address: '/info_whois' },
-        { value: 'JBoss 检测', address: '/exp_jboss_scan' },
-        { value: 'Jenkins 爆破', address: '/exp_jenkins_check' },
-        { value: 'URL 收集', address: '/tool_url' },
-        { value: 'MD5 查询', address: '/tool_md5' },
-        { value: '密码生成器', address: '/tool_pwd' },
-        { value: '加解密', address: '/tool_decrypt' },
-      ];
     },
     handleSelect(item) {
       console.log(item);
-      this.$router.push({ path: item.address });
-      Bus.$emit('activeIndex', item.address);
+      this.$router.push({ path: item.path });
+      Bus.$emit('activeIndex', item.path);
     },
   },
   mounted() {
-    this.restaurants = this.loadAll();
+    this.pageroutes = getPageRoutes();
   },
 };
 </script>
--- src/router/view.js
+++ src/router/view.js
@@ -2,21 +2,21 @@ import { importPage } from '../utils'
 
 // 页面路由
 export default () => [
-	{ name: 'CdnDetect', path: '/info_cdn', component: importPage("CdnDetect") },
-	{ name: 'WafCheck', path: '/info_waf', component: importPage("WafCheck") },
-	{ name: 'EmailGather', path: '/info_email', component: importPage("EmailGather") },
-	{ name: 'SubDomain', path: '/info_subdomain', component: importPage("SubDomain") },
-	{ name: 'Whois', path: '/info_whois', component: importPage("Whois") },
+  { name: 'CdnDetect', path: '/info_cdn', component: importPage("CdnDetect"), value: 'Cdn 检测' },
+  { name: 'WafCheck', path: '/info_waf', component: importPage("WafCheck"), value: 'Waf 检测' },
+  { name: 'EmailGather', path: '/info_email', component: importPage("EmailGather"), value: '邮箱地址收集' },
+  { name: 'SubDomain', path: '/info_subdomain', component: importPage("SubDomain"), value: '子域名爆破' },
+  { name: 'Whois', path: '/info_whois', component: importPage("Whois"), value: 'WhoIs 反查', },
 
-	{ name: 'JBossScan', path: '/exp_jboss_scan', component: importPage("JBossScan"), },
-	{ name: 'JBossShell', path: '/exp_jboss_shell', component: importPage("JBossShell") },
+  { name: 'JBossScan', path: '/exp_jboss_scan', component: importPage("JBossScan"), value: 'JBoss 检测', },
+  { name: 'JBossShell', path: '/exp_jboss_shell', component: importPage("JBossShell"), value: ''},
 
-	{ name: 'JenkinsCheck', path: '/exp_jenkins_check', component: importPage("JenkinsCheck"), },
-	{ name: 'JenkinsShell', path: '/exp_jenkins_shell', component: importPage("JenkinsShell") },
+  { name: 'JenkinsCheck', path: '/exp_jenkins_check', component: importPage("JenkinsCheck"), value: 'Jenkins 爆破', },
+  { name: 'JenkinsShell', path: '/exp_jenkins_shell', component: importPage("JenkinsShell"), value:''},
 
-	{ name: 'UrlGather', path: '/tool_url', component: importPage("UrlGather") },
-	{ name: 'Md5Query', path: '/tool_md5', component: importPage("Md5Query") },
-	{ name: 'PwdGenerator', path: '/tool_pwd', component: importPage("PwdGenerator") },
-	{ name: 'Decrypt', path: '/tool_decrypt', component: importPage("Decrypt") },
+  { name: 'UrlGather', path: '/tool_url', component: importPage("UrlGather"), value: 'URL 收集', },
+  { name: 'Md5Query', path: '/tool_md5', component: importPage("Md5Query"), value: 'MD5 查询', },
+  { name: 'PwdGenerator', path: '/tool_pwd', component: importPage("PwdGenerator"), value: '密码生成器', },
+  { name: 'Decrypt', path: '/tool_decrypt', component: importPage("Decrypt"), value: '加解密', },
 
 ]
